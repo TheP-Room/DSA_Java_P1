06d54182704499264a9358c4f7633421a7c18dc7
Devanshu <dev.ballam@gmail.com>
2025-10-20 01:53:04 +0000

    Simplify StackLinked by removing min tracking
    Removed min tracking functionality from StackLinked.

--- StackLinked.java
+++ StackLinked.java
@@ -2,21 +2,14 @@ import java.util.Arrays;
 
 public class StackLinked {
     private final LinkedList linkedList = new LinkedList();
-    private int min;
-    private int lastMin;
 
     public void push(int item){
-        if (linkedList.size() == 0)
-            min = item;
-        lastMin = min;
-        if (item < min) min = item;
         linkedList.addLast(item);
     }
 
     public int pop(){
         var value = linkedList.getKThFromEnd(1);
         linkedList.removeLast();
-        if (value == min) min = lastMin;
         return value;
     }
 
@@ -28,12 +21,9 @@ public class StackLinked {
         return linkedList.size() == 0;
     }
 
-    public int getMin(){
-        return min;
-    }
-
     @Override
     public String toString(){
         return Arrays.toString(linkedList.toArray());
     }
 }
+
