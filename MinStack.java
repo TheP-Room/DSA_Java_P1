a22f00983a6e3c7c89c58d9d6ec1f423363edf39
Devanshu <dev.ballam@gmail.com>
2025-10-20 01:51:27 +0000

    Refactor MinStack to improve min tracking (Constant Time)

--- MinStack.java
+++ MinStack.java
@@ -1,64 +1,67 @@
 import java.util.Arrays;
 
-public class MinStack {
-    private int[] stack = new int[3];
-    private int size;
-    private int min;
-    private int lastMin;
-
-    private void stretchIfRequired(){
-        if (size == stack.length) {
-            var temp = new int[2 * stack.length];
-            for (int i = 0; i<size; i++)
+public class MinStack
+{
+    private int[] stack = new int[5];
+    private int size = 5;
+    private int count;
+    private int min[] = new int[size];
+    private int minCount;
+    
+    private void resize() {
+        if (count == size) {
+            var temp = new int[2 * size];
+            for (int i = 0; i<count; i++)
                 temp[i] = stack[i];
             stack = temp;
+            size *= 2;
         }
     }
-
-    private void pressIfRequired(){
-        if (size == stack.length/2){
-            var temp = new int[stack.length/2];
-            for (int i = 0; i<size; i++)
-                temp[i] = stack[i];
-            stack = temp;
-        }
-    }
-
-    public void push(int item){
-        stretchIfRequired();
-        if (size == 0) min = item;
-        lastMin = min;
-        if (item < min) min = item;
-        stack[size++] = item;
-    }
-
-    public int pop(){
-        if (size == 0)
+    
+    private void checkNull() {
+        if (count == 0)
             throw new IllegalStateException();
-        int value = stack[size-1];
-        size--;
-        pressIfRequired();
-        if (value == min)
-            min = lastMin;
-        return value;
     }
-
-    public int peek(){
-        if (size == 0)
+    
+    public void push(int item) {
+        resize();
+        if (count == 0)
+            min[minCount++] = item;
+        else
+            if (item < min[minCount-1])
+                min[minCount++] = item;
+        stack[count++] = item;
+    }
+    
+    public int pop() {
+        checkNull();
+        var trash = stack[--count];
+        if (trash == min[minCount - 1])
+            minCount--;
+        return trash;
+    }
+    
+    public int peek() {
+        checkNull();
+        return stack[count - 1];
+    }
+    
+    public boolean isEmpty() {
+        return count == 0;
+    }
+    
+    public int getSize() {
+        return count;
+    }
+    
+    public int getMin() {
+        if (minCount == 0)
             throw new IllegalStateException();
-        return stack[size-1];
+        return min[minCount-1];
     }
-
-    public boolean isEmpty(){
-        return size == 0;
-    }
-
-    public int min(){
-        return min;
-    }
-
+    
     @Override
-    public String toString(){
-        return Arrays.toString(Arrays.copyOfRange(stack,0,size));
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(stack,0,count));
     }
 }
