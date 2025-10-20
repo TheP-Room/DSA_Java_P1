9af47a30b8e37479e49739077d255d031a74aa41
Devanshu <dev.ballam@gmail.com>
2025-10-20 00:22:52 +0000

    Refactor Array class to use generics

--- Array.java
+++ Array.java
@@ -1,111 +1,87 @@
-public class Array {
+import java.util.Arrays;
 
-    // Custom Made Dynamic Array
-
-    private int[] list;
+public class Array<T>
+{
+    private T[] array = (T[]) new Object[5];
     private int count;
-
-    public Array(int size){
-        list = new int[size];
-    }
-
-    public void insert(int item){
-        if (list.length == count){
-            int[] temp = new int[count+1];
+    private int size = 5;
+    
+    private void resizeIfRequired() {
+        if (count == size) {
+            var temp = (T[]) new Object[2 * size];
             for (int i = 0; i<count; i++)
-                temp[i] = list[i];
-            list = temp;
+                temp[i] = array[i];
+            array = temp;
+            size *= 2;
         }
-        list[count++] = item;
     }
-
-    public void removeAt(int index){
+    
+    public void insert(T item) {
+        resizeIfRequired();
+        array[count++] = item;
+    }
+    
+    private void validateIndex(int index) {
         if (index < 0 || index >= count)
             throw new IllegalArgumentException();
-
-        for (int i = index; i < count; i++) {
-            if (i+1 >= count)
-                continue;
-            list[i] = list[i + 1];
-        }
-
-        count -= 1;
     }
-
-    public int indexOf(int item){
-        for (int i = 0; i<count; i++)
-            if (list[i] == item)
+    
+    public void removeAt(int index) {
+        validateIndex(index);
+        for (int i = index; i<count; i++)
+            array[i] = array[i+1];
+        count--;
+    }
+    
+    public int indexOf(T item) {
+        for (int i = 0; i<count ; i++)
+            if (array[i] == item)
                 return i;
         return -1;
     }
-
-    public int max(){
-        int max = 0;
-        for (int item : list)
-            if(max < item)
-                max = item;
-        return max;
-    }
-
-    public int itemAt(int index){
-        if (index >= count || index < 0) throw new IllegalArgumentException();
-        return list[index];
-    }
-
-    public int getCount(){
+    
+    public int getSize() {
         return count;
     }
-
-    private int minimum(Array first,Array other){
-        if (first.getCount() >= other.getCount()){
-            return getCount();
-        }
-        return other.getCount();
+    
+    public T itemAt(int index) {
+        validateIndex(index);
+        return array[index];
     }
-
-    public Array intersect(Array other){
-        Array intersection = new Array(minimum(this,other));
-        for (int items : list)
-            if (other.indexOf(items) >= 0)
-                intersection.insert(items);
-        return intersection;
+    
+    private T[] getArray() {
+        return array;
     }
-
-    public void reverse(){
-        int[] temp = new int[count];
-        for (int i = 0; i<count; i++)
-            temp[i] = list[count - i - 1];
-
-        list = temp;
+    
+    public Array intersect(Array other) {
+        Array minInTwo = (count > other.getSize()) ? other : this;
+        Array intersection = new Array();
+        T[] toIterate = (minInTwo == this) ? array : (T[])other.getArray();
+        Array toCheck = (toIterate == array) ? other : this;
+        for (var item : toIterate)
+            if (toCheck.indexOf(item) > -1)
+                intersection.insert(item);
+        return (intersection.getSize() == 0) ? null : intersection;
     }
-
-    public void replaceAt(int item,int index){
-        if (index < 0 || index >= count)
-            throw new IllegalArgumentException();
-        list[index] = item;
+    
+    public void reverse() {
+        var temp = (T[]) new Object[size];
+        for (int i = 0 ; i<count; i++)
+            temp[count-i-1] = array[i];
+        array = temp;
     }
-
-    public void insertAt(int item, int index){
-
-        if (index < 0 || index >= count)
-            throw new IllegalArgumentException();
-
-        int[] temp = new int[count+1];
-        int tempNo = 0;
-        for (int i = 0; i<count+1; i++){
-            if (i == index){
-                temp[i] = item;
-                tempNo++;
-                continue;
-            }
-            temp[i] = list[i-tempNo];
-        }
+    
+    public void insertAt(T item, int index) {
+        validateIndex(index);
+        resizeIfRequired();
+        for (int i = count; i>index; i--)
+            array[i] = array[i-1];
+        array[index] = item;
         count++;
-        list = temp;
     }
-
-    public void print(){
-        for (int i = 0; i<count ; i++)
-            System.out.println(list[i]);
+    
+    @Override
+    public String toString() {
+        return Arrays.toString(Arrays.copyOfRange(array,0,count));
     }
 }
